32665dbbfa61e27f02749a8f9a38e596263f2d60
ali-samadyar <samadyar@live.com>
2024-02-27 17:17:16 +0000

    mini-commit

--- ProjectX/sslmonitor/urls.py
+++ ProjectX/sslmonitor/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# app_name = 'sslmonitor'
+
 urlpatterns = [
     path('', views.sslchecker_function, name='sslchecker_function'),
     path('remove_ssl_certificate/', views.remove_ssl_certificate, name='remove_ssl_certificate'),
