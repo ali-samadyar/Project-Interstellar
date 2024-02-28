26d7f29d67cbf679a87d9462b5802b2a61fb0911
ali-samadyar <samadyar@live.com>
2024-02-28 22:29:35 +0000

    DeviceHub ADD module done

--- ProjectX/deviceHub/urls.py
+++ ProjectX/deviceHub/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.device_hub, name='devicehub'),
-    path('add-device/', views.add_device, name='add_device'),
-    path('search/', views.search, name='search'),
-    path('device_list_all/', views.device_list_all, name='device_list_all'),
-    path('device_list_all/<int:id>/', views.get_device_detail, name='get_device_detail'),
-    path('edit_device/<int:id>/', views.edit_device, name='edit_device'),
-    path('fetch_device/<int:id>/', views.fetch_device, name='fetch_device'),
+    path('add_device/', views.add_device, name='add_device'),
+
+    # path('search/', views.search, name='search'),
+    # path('device_list_all/', views.device_list_all, name='device_list_all'),
+    # path('device_list_all/<int:id>/', views.get_device_detail, name='get_device_detail'),
+    # path('edit_device/<int:id>/', views.edit_device, name='edit_device'),
+    # path('fetch_device/<int:id>/', views.fetch_device, name='fetch_device'),
 
 ]
--- ProjectX/static/js/device_add.js
+++ ProjectX/static/js/device_add.js
@@ -7,10 +7,10 @@ function closeAddDeviceModal() {
     document.getElementById('addDeviceModal').style.display = 'none';
 }
 
-// Close the modal if the user clicks outside of it
-window.onclick = function (event) {
-    var modal = document.getElementById('addDeviceModal');
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-}
+// // Close the modal if the user clicks outside of it
+// window.onclick = function (event) {
+//     var modal = document.getElementById('addDeviceModal');
+//     if (event.target == modal) {
+//         modal.style.display = 'none';
+//     }
+// }
