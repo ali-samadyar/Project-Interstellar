582f445624c1d6e976c106c734f71d428607602b
samadyar-ali <ali.samadyar@gmail.com>
2024-03-03 17:02:43 +0000

    bgpMonitor db added/

--- ProjectX/bgpMonitor/models.py
+++ ProjectX/bgpMonitor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class BGPCheckResult(models.Model):
+    as_number = models.CharField(max_length=255)
+    as_name = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.as_number} - {self.as_name}'
