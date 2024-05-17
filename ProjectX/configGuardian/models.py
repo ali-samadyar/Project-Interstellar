c1195bccd42d702a09418032699679145036fbae
samadyar-ali <ali.samadyar@gmail.com>
2024-05-17 17:01:36 +0100

    Minor update

--- ProjectX/configGuardian/models.py
+++ ProjectX/configGuardian/models.py
@@ -8,4 +8,4 @@ class ConfigBackup(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.device_ip 
+        return f'{self.device_ip} - {self.timestamp}'
